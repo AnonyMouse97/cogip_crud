092cae396ce329b2fa1724da08524dcfea26b3f8
letocart samuel <samuel.le.belgique@gmail.com>
2021-04-19 14:17:50 +0200

    fix env vars

--- src/Model/db_connect.php
+++ src/Model/db_connect.php
@@ -2,14 +2,16 @@
 
 declare(strict_types=1);
 
-//env vars
-$dbUsername = getenv('DB_NAME');
-$dbPassword = getenv('DB_PASSWORD');
+
 
 class ConnectionManager
 {
     protected function connect_db()
     {
+        //env vars
+        $dbUsername = getenv('DB_NAME');
+        $dbPassword = getenv('DB_PASSWORD');
+        
         try {
             $db = new PDO("mysql:host=remotemysql.com;dbname=$dbUsername;port=3306", "$dbUsername", "$dbPassword"  );
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
