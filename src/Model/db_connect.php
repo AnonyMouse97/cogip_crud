9580b0ff5197c901f8bb0f8cc18f5c6d55ccb26c
letocart samuel <samuel.le.belgique@gmail.com>
2021-04-19 11:54:48 +0200

    en vars

--- src/Model/db_connect.php
+++ src/Model/db_connect.php
@@ -2,12 +2,16 @@
 
 declare(strict_types=1);
 
+//env vars
+$dbUsername = getenv('DB_NAME');
+$dbPassword = getenv('DB_PASSWORD');
+
 class ConnectionManager
 {
     protected function connect_db()
     {
         try {
-            $db = new PDO("mysql:host=remotemysql.com;dbname=" . $_ENV['id'] . ";port=3306", $_ENV['id'], $_ENV['password']);
+            $db = new PDO("mysql:host=remotemysql.com;dbname=$dbUsername;port=3306", "$dbUsername", "$dbPassword"  );
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $db;
         } catch (Exception $e) {
